3506fc328ad5570e1e73012c60cc63a24163d6d0
Arkhipov Valerii <v.arkhipov@aaa0.ru>
2019-06-30 22:20:59 +0300

    + static 2

---
+++ precache-manifest.00e015099af0237c50f6a361a1f32f71.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a6a399670929f939c580a67efc5e5c3",
+    "url": "/meta/static/media/speaker.1a6a3996.svg"
+  },
+  {
+    "revision": "9c069d8075be86948683",
+    "url": "/meta/static/css/main.6b492564.chunk.css"
+  },
+  {
+    "revision": "e5384e24ffdeb00db2f9",
+    "url": "/meta/static/js/runtime~main.f53e4213.js"
+  },
+  {
+    "revision": "3efa138ac63da3c780e4",
+    "url": "/meta/static/js/2.cd3fc5e1.chunk.js"
+  },
+  {
+    "revision": "4343fe6dddb1317aa670f8d6272a0428",
+    "url": "/meta/static/media/fire_alert.4343fe6d.png"
+  },
+  {
+    "revision": "9c069d8075be86948683",
+    "url": "/meta/static/js/main.5c9ecaf7.chunk.js"
+  },
+  {
+    "revision": "f83bdec8238d8902c1722f30b968f1d0",
+    "url": "/meta/static/media/attention.f83bdec8.wav"
+  },
+  {
+    "revision": "6ec24ef8794fbea384aed92cb226a06c",
+    "url": "/meta/static/media/fire.6ec24ef8.wav"
+  },
+  {
+    "revision": "6b6a283da2345df612975d46689761f5",
+    "url": "/meta/static/media/gong.6b6a283d.wav"
+  },
+  {
+    "revision": "05eb3326f6bc4a6ccfbc3e309adae302",
+    "url": "/meta/static/media/sirena.05eb3326.wav"
+  },
+  {
+    "revision": "248f909398b2d2b9327828a9c0efb919",
+    "url": "/meta/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ebe474219d8fa0c037a2caf5f17e7bf.js"
+  "/meta/precache-manifest.00e015099af0237c50f6a361a1f32f71.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/meta/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
